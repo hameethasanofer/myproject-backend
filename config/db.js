4165c9b952c1fb2b8cf86f95c9d17390929ba4a8
hameethasanofer <hameethasanofer610@gmail.com>
2026-05-15 15:59:16 -0400

    fix

--- config/db.js
+++ config/db.js
@@ -2,11 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in environment variables");
-    }
-
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 5000,
+    });
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
