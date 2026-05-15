4f33004b3640a2847e6d2f9a754e5832a821079e
hameethasanofer <hameethasanofer610@gmail.com>
2026-05-15 11:15:45 -0400

    fixed root route

--- server.js
+++ server.js
@@ -5,39 +5,41 @@ import morgan from 'morgan';
 import connectDB from './config/db.js';
 import jobRoutes from './routes/jobRoutes.js';
 
-
 dotenv.config();
-
 
 connectDB();
 
 const app = express();
 
-
 app.use(express.json());
 
-
 app.use(cors());
-
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Root Route
+app.get('/', (req, res) => {
+  res.send('API Running...');
+});
 
+// Routes
 app.use('/api/jobs', jobRoutes);
 
-
+// 404 Middleware
 app.use((req, res, next) => {
   res.status(404).json({
     message: `Not Found - ${req.originalUrl}`
   });
 });
 
-
+// Error Middleware
 app.use((err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
   res.status(statusCode);
+
   res.json({
     message: err.message,
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
@@ -47,5 +49,9 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${process.env.NODE_ENV || 'development'} mode on port ${PORT}`);
+  console.log(
+    `Server running in ${
+      process.env.NODE_ENV || 'development'
+    } mode on port ${PORT}`
+  );
 });
